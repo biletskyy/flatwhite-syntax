6c3ba721ee132536fa6288741cd7d257b12d886d
Dmitry Biletskyy <dima@biletskyy.com>
2016-09-22 18:16:54 +0300

    adds Rust lang support

---
+++ code-samples/rust-sample.rs
@@ -0,0 +1,205 @@
+use std::fmt::{self, Formatter, Display};
+
+fn main() {
+    // Integer addition
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // Integer subtraction
+    println!("1 - 2 = {}", 1i32 - 2);
+    // TODO ^ Try changing `1i32` to `1u32` to see why the type is important
+
+    // Short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability!
+    println!("One million is written as {}", 1_000_000u32);
+}
+
+struct City {
+    name: &'static str,
+    // Latitude
+    lat: f32,
+    // Longitude
+    lon: f32,
+}
+
+impl Display for City {
+    // `f` is a buffer, this method must write the formatted string into it
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        // `write!` is like `format!`, but it will write the formatted string
+        // into a buffer (the first argument)
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+fn main() {
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display
+        println!("{:?}", *color)
+    }
+}
+
+
+use std::fmt;
+
+#[derive(Debug)]
+struct Vector2D {
+    x: isize,
+    y: isize,
+}
+
+impl fmt::Display for Vector2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // The `f` value implements the `Write` trait, which is what the
+        // write! macro is expecting. Note that this formatting ignores the
+        // various flags provided to format strings.
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+// Different traits allow different forms of output of a type. The meaning
+// of this format is to print the magnitude of a vector.
+impl fmt::Binary for Vector2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let magnitude = (self.x * self.x + self.y * self.y) as f64;
+        let magnitude = magnitude.sqrt();
+
+        // Respect the formatting flags by using the helper method
+        // `pad_integral` on the Formatter object. See the method
+        // documentation for details, and the function `pad` can be used
+        // to pad strings.
+        let decimals = f.precision().unwrap_or(3);
+        let string = format!("{:.*}", decimals, magnitude);
+        f.pad_integral(true, "", &string)
+    }
+}
+
+fn main() {
+    let myvector = Vector2D { x: 3, y: 4 };
+
+    println!("{}", myvector);       // => "(3, 4)"
+    println!("{:?}", myvector);     // => "Vector2D {x: 3, y:4}"
+    println!("{:10.3b}", myvector); // => "     5.000"
+}
+
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
+
+// Create an `enum` to classify someone. Note how both names
+// and type information together specify the variant:
+// `Engineer != Scientist` and `Height(i32) != Weight(i32)`. Each
+// is different and independent.
+enum Person {
+    // An `enum` may either be `unit-like`,
+    Engineer,
+    Scientist,
+    // like tuple structs,
+    Height(i32),
+    Weight(i32),
+    // or like structures.
+    Info { name: String, height: i32 }
+}
+
+// A function which takes a `Person` enum as an argument and
+// returns nothing.
+fn inspect(p: Person) {
+    // Usage of an `enum` must cover all cases (irrefutable)
+    // so a `match` is used to branch over it.
+    match p {
+        Person::Engineer  => println!("Is an engineer!"),
+        Person::Scientist => println!("Is a scientist!"),
+        // Destructure `i` from inside the `enum`.
+        Person::Height(i) => println!("Has a height of {}.", i),
+        Person::Weight(i) => println!("Has a weight of {}.", i),
+        // Destructure `Info` into `name` and `height`.
+        Person::Info { name, height } => {
+            println!("{} is {} tall!", name, height);
+        },
+    }
+}
+
+fn main() {
+    let person   = Person::Height(18);
+    let amira    = Person::Weight(10);
+    // `to_owned()` creates an owned `String` from a string slice.
+    let dave     = Person::Info { name: "Dave".to_owned(), height: 72 };
+    let rebecca  = Person::Scientist;
+    let rohan    = Person::Engineer;
+
+    inspect(person);
+    inspect(amira);
+    inspect(dave);
+    inspect(rebecca);
+    inspect(rohan);
+}
+
+fn main() {
+    let number = 13;
+    // TODO ^ Try different values for `number`
+
+    println!("Tell me about {}", number);
+    match number {
+        // Match a single value
+        1 => println!("One!"),
+        // Match several values
+        2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
+        // Match an inclusive range
+        13...19 => println!("A teen"),
+        // Handle the rest of cases
+        _ => println!("Ain't special"),
+    }
+
+    let boolean = true;
+    // Match is an expression too
+    let binary = match boolean {
+        // The arms of a match must cover all the possible values
+        false => 0,
+        true => 1,
+        // TODO ^ Try commenting out one of these arms
+    };
+
+    println!("{} -> {}", boolean, binary);
+}
+
+let mut array: [i32; 3] = [0; 3];
+
+array[1] = 1;
+array[2] = 2;
+
+assert_eq!([1, 2], &array[1..]);
+
+// This loop prints: 0 1 2
+for x in &array {
+    print!("{} ", x);
+}
